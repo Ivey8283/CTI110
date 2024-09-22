703debd03e34beecfc33f0bb890ee914ba2b3b6d
Ivey8283 <zacharyi8283@student.faytechcc.edu>
2024-09-22 00:04:47 -0400

    Add files via upload

---
+++ P1LAB1_IveyZachary.py
@@ -0,0 +1,13 @@
+# Zachary Ivey
+# 9/21/2024
+# P1LAB1
+# Shows us how to create a python program using IDLE.
+print('Enter your first name:', end=' ')
+firstname = input()
+print('Enter your first name', firstname)
+
+print('Enter your last name:', end=' ')
+lastname = input()
+print('Enter your last name:', lastname)
+
+print('Hello,', firstname, lastname, '! Welcome to CTI-110')
