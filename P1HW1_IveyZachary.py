7b5cb6ebc4de8df33023a76530b9e23f0ba276d1
Ivey8283 <zacharyi8283@student.faytechcc.edu>
2024-09-27 22:48:51 -0400

    P1HW1

---
+++ P1HW1_IveyZachary.py
@@ -0,0 +1,24 @@
+#Zachary
+#9/27/2024
+#P1HW1
+#Using python to collect information and process information to display to the user
+print('-----Calculating Exponents-----')
+base_value = int(input('Enter an integer as the base value: '))
+
+exponent = int(input('Enter an integer as the exponent: '))
+
+end_value = base_value ** exponent
+
+print(base_value, 'raised to the power of', exponent, 'is', end_value, '!!') 
+
+
+print('-----Addition and Subtraction -----')
+int1 = int(input('Enter a starting integer: '))
+
+int2 = int(input('Enter an integer to add: '))
+
+int3 = int(input('Enter an integer to subtract: '))
+
+int4 = int1 + int2 - int3
+
+print( int1, '+', int2, '-', int3, 'is equal to', int4)
