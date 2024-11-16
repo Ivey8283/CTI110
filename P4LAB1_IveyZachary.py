8fec35201966410bd2ba14ad405d1651297eb569
Ivey8283 <zacharyi8283@student.faytechcc.edu>
2024-11-15 20:57:42 -0500

    Add files via upload

---
+++ P4LAB1_IveyZachary.py
@@ -0,0 +1,26 @@
+import turtle
+
+turtle.shape("turtle")
+turtle.color("red")
+
+for i in range(4):
+    turtle.forward(50)
+    turtle.right(90)
+    
+turtle.reset()
+
+turtle.color("blue")
+
+for i in range(3):
+    turtle.forward(80)
+    turtle.right(120)
+
+turle.reset()
+
+Turtle.color("green")
+
+turtle.forward(90)
+turtle.left(55)
+turtle.forward(90)
+turtle.right(55)
+turtle.forward(90)
