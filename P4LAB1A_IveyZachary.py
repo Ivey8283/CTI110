dbe7bf5c3f55bd60f77f126374a535d2df55d5d8
Ivey8283 <zacharyi8283@student.faytechcc.edu>
2024-11-15 20:58:05 -0500

    P4LABA

---
+++ P4LAB1A_IveyZachary.py
@@ -0,0 +1,16 @@
+import turtle
+
+turtle.shape("turtle")
+turtle.color("red")
+
+for i in range(4):
+    turtle.forward(50)
+    turtle.right(90)
+    
+turtle.reset()
+
+turtle.color("blue")
+
+for i in range(3):
+    turtle.forward(80)
+    turtle.right(120)
